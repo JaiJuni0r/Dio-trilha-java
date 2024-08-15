cbf9575eca30406aff03bef1ddcc7f1e3eac9da1
Jailson Junior <jailson_junior123@hotmail.com>
2024-08-15 08:52:29 -0300

    Aprendendo sobre anatomia de classes

--- meu-primeiro-projeto-java/src/MinhaClasse.java
+++ meu-primeiro-projeto-java/src/MinhaClasse.java
@@ -2,7 +2,17 @@ public class MinhaClasse {
     
     public static void main(String[] args) {
 
+        String primeiroNome = "Jailson";
+        String segundoNome = "Junior";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
         
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
     }
 
 }

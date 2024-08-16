115da55ba5129f7c75ea05020e2f9a9b731f9a39
Jailson Junior <jailson_junior123@hotmail.com>
2024-08-16 07:23:24 -0300

    Tipos e variaveis

--- meu-primeiro-projeto-java/tipos-variaveis/src/TiposVariaveis.java
+++ meu-primeiro-projeto-java/tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,27 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println(" aasjapfjapsojfopajfsa");
+
     }
 }
+
+
+// Em java, existem 8 tipos de tipos primitivos (São valores brutos e são armazenados diretamente na memoria.)
+
+// São eles: 
+// byte = 1 byte de memoria. Pode armazenar entre -128 a 127
+// short = 2 byte de memoria. Pode armazenar entre -32.768 a 32.767
+// int = 4 byte de memoria. Pode armazenar entre -2.147.483.648 a 2.147.483.647 (Sendo o mais comum de ser usado, devido a compatibilidade da JVM)
+// long  = 8 byte de memoria. Pode armazenar entre -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807
+
+// float= 4 byte de memoria. Pode armazenar entre -3,4028E+38 a 3,4028E+38
+// double = 8 byte de memoria. Pode armazenar entre -1,7976E+308 a 1,7976E+308 (Assim como o int, é mais usado em projetos)
+
+// boolean
+// char
+
+// Como se declara uma variavel? Seguindo a convesão, segue uma estrutura padrão de <Tipo> <nomeVariavel> <atribuicaoDeValorOpicional>
+
+// Exemplo:
+// int idade; tipo "int", nome "idade", com nenhum valor
+// int anoFabricacao = 2021; tipo "int", nome "anoFabricacao", com valor "2021"
